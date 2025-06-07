70c42e398e66c6f4423a070762b88f613c024186
DanielChitumba7 <DanielChitumba@outlook.com>
2025-06-07 13:56:55 +0100

    feat: Endpoints Implementations

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,24 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/tasks', [TaskController::class,'index']);
+    Route::get('/tasks/{id}', [TaskController::class,'show']);
+    Route::post('/tasks', [TaskController::class,'store']);
+    Route::put('/tasks/{id}', [TaskController::class,'update']);
+    Route::delete('/tasks/{id}', [TaskController::class,'destroy']);
+    Route::get('/tasks/status/{status}', [TaskController::class,'filterByStatus']);
+
+    Route::post('/user/logout', [AuthController::class,'logout']);
+});
+
+
+
+ Route::post('/user/register', [AuthController::class,'register']);
+ Route::post('/user/login', [AuthController::class,'login']);
+ Route::post('/user/forgot-password', [AuthController::class,'forgotPassword']);
